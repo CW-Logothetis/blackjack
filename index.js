bf82468b154f0fa36c8f71166d6828d882df1674
CW-Logothetis <cwlogo@pm.me>
2021-07-06 17:43:00 +0300

    update newCard button and text

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ let cardBtn = document.getElementById("card-btn")
 
 let cards = []
 let sum = 0
+let userInGame = false
+let hasBlackjack = false
 
 playerEl.textContent = `${user.name}: ${user.chips}€`
 
@@ -22,51 +24,52 @@ function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function renderGame() {
+     cardsEl.textContent = " "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+  if (sum <= 20) {
+        statusEl.textContent = "Do you want to draw a new card?"
+        }
+        else if (sum === 21) {
+            statusEl.textContent = "You've got Blackjack!"
+            hasBlackjack = true
+        }
+        else {
+            statusEl.textContent = "You lose! Do you want to play again? Hit Start Game."
+            userInGame = false
+        }
+}
+
 startBtn.addEventListener("click", function() {
+    userInGame = true
     cards = []
     let firstCard = getRandom(1, 11)
     let secondCard = getRandom(1, 11)
     cards = [firstCard, secondCard]   
     sum = firstCard + secondCard
-    // cardsEl.textContent = `${firstCard} ${secondCard}`
     cardsEl.textContent = " "
     for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += cards[i] + " "
-    }
+     }
     sumEl.textContent = sum
-    if (sum <= 20) {
-        statusEl.textContent = "Do you want to draw a new card?"
-        }
-        else if (sum === 21) {
-            statusEl.textContent = "You've got Blackjack!"
-        }
-        else {
-            statusEl.textContent = "You lose!"
-        }
+    renderGame()
 })
 
-// function renderGame() {
-//     cardsEl.textContent = "Cards: "
-//     for (let i = 0; i < cards.length; i++) {
-//         cardsEl.textContent += cards[i] + " "
-//     }
+
 
 cardBtn.addEventListener("click", function() {
-    let newCard = getRandom(1, 11)
-        // cardsEl.textContent += ` ${newCard}`
-        // cards = 
-       
+    if (userInGame === true && hasBlackjack === false) {
+        let newCard = getRandom(1, 11)
         cards.push(newCard)
-    cardsEl.textContent += newCard + " "
+        cardsEl.textContent += newCard + " "
         sum += newCard
         sumEl.innerHTML = sum
-    if (sum < 21) {
-        statusEl.textContent = "Do you want to draw a new card?"
-        }
-        else if (sum === 21) {
-            statusEl.textContent = "You've got Blackjack!"
-        }
-        else {
-            statusEl.textContent = "You lose!"
+        renderGame()
+    }
+    else {
+        let newCard = ""
+        statusEl.textContent = "Do you want to play again? Hit Start Game"
         }
 })
