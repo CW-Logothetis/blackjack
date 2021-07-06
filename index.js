49078b4b61bcc1a2ca8edcbb8d67b8ad3ca9630c
CW-Logothetis <cwlogo@pm.me>
2021-07-06 15:00:28 +0300

    Correct issue- start game didn't give new cards

--- index.js
+++ index.js
@@ -1,14 +1,74 @@
+const user = {
+    name: "Chris",
+    chips: 200
+} 
+
 let statusEl = document.getElementById("status-display")
 let cardsEl = document.getElementById("cards-display")
 let sumEl = document.getElementById("sum-display")
 let playerEl = document.getElementById("player-display")
+let startBtn = document.getElementById("start-btn")
+let cardBtn = document.getElementById("card-btn")
+
+let firstCard = getRandom(1, 11)
+let secondCard = getRandom(1, 11)
+
+// let sum = 0
+
+playerEl.textContent = `${user.name}: ${user.chips}€`
+
+// let cards = []
 
 function getRandom(min, max) {
     min = Math.ceil(min);
-    max =Math.floor(max);
+    max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
-console.log(getRandom(1,11))
 
-getRandom()
+startBtn.addEventListener("click", function() {
+    // let firstCard = getRandom(1, 11)
+    // let secondCard = getRandom(1, 11)
+    cards = [firstCard, secondCard]   
+    sum = firstCard + secondCard
+    // cardsEl.textContent = `${firstCard} ${secondCard}`
+    cardsEl.textContent = " "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    sumEl.textContent = sum
+    if (sum <= 20) {
+        statusEl.textContent = "Do you want to draw a new card?"
+        }
+        else if (sum === 21) {
+            statusEl.textContent = "You've got Blackjack!"
+        }
+        else {
+            statusEl.textContent = "You lose!"
+        }
+})
 
+// function renderGame() {
+//     cardsEl.textContent = "Cards: "
+//     for (let i = 0; i < cards.length; i++) {
+//         cardsEl.textContent += cards[i] + " "
+//     }
+
+cardBtn.addEventListener("click", function() {
+    let newCard = getRandom(1, 11)
+        // cardsEl.textContent += ` ${newCard}`
+        // cards = 
+       
+        cards.push(newCard)
+    cardsEl.textContent += newCard + " "
+        sum += newCard
+        sumEl.innerHTML = sum
+    if (sum < 21) {
+        statusEl.textContent = "Do you want to draw a new card?"
+        }
+        else if (sum === 21) {
+            statusEl.textContent = "You've got Blackjack!"
+        }
+        else {
+            statusEl.textContent = "You lose!"
+        }
+})
